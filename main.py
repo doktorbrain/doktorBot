bc745e9780857f39f480adc76f71fcd0ae4eea5e
doktor brain <doktorbr4in@gmail.com>
2021-01-03 15:58:23 +0000

    added roulette

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+import random
 
 #https://discord.com/api/oauth2/authorize?&client_id=795213505162248202&scope=bot&permissions=8
 class MyClient(discord.Client):
@@ -17,7 +18,34 @@ class MyClient(discord.Client):
     elif message.content.startswith("$help"):
       await message.channel.send("Was ich weiß: $help, spiel")
     elif message.content.startswith("spiel"):
-      await message.channel.send("ich überlege mir noch eins...")
+      await message.channel.send("$roulette [BID] \n [BID]->red,black,number")
+    elif message.content.startswith("$roulette"):
+      bid = message.content.split(' ')[1]
+      bid_param = -3
+      if bid.lower() == "black":
+        bid_param = -1
+      if bid.lower() == "red":
+        bid_param = -2
+      else:
+        try:
+          bid_param = int(bid)
+        except:
+          bid_param = -3
+      if bid_param == -3:
+        await message.channel.send("BID Input not Valid")
+        return
+      result = random.randint(0,36)
+      print(result)
+      if bid_param == -1:
+        won = result%2 == 0 and not result == 0
+      elif bid_param == -2:
+        won = result%2 == 1
+      else:
+        won = result == bid_param
+      if won:
+        await message.channel.send("$$$ Gewonnen! $$$")
+      else:
+        await message.channel.send("Leider verloren :-( - "+str(result))
 
   async def on_message_delete(self,message):
     print("deleted message: "+ message.content)
